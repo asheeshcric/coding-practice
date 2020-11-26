73ca665623eb88ef1bd3d4ca8435974c137339ee
asheeshcric <ashiz2013@gmail.com>
2020-11-25 20:33:30 -0600

    Add bottom-up soluton for subset sum problem

--- dynamic_programming/1_knapsack_based/4_subset_sum_problem.py
+++ dynamic_programming/1_knapsack_based/4_subset_sum_problem.py
@@ -17,6 +17,9 @@ then return True, else return False
 
 
 def subset_sum(numbers, target, n):
+    """
+    Recursive approach
+    """
     if n == 0 or target == 0:
         return 0
 
@@ -29,12 +32,42 @@ def subset_sum(numbers, target, n):
         return subset_sum(numbers, target, n-1)
 
 
+def subset_sum_bottom_up(numbers, target, n):
+    dp = [[False for col in range(target+1)] for row in range(n+1)]
+
+    if target == 0:
+        return True
+
+    if n == 0:
+        return False
+
+    for i in range(n+1):
+        for j in range(target+1):
+            if i == 0:
+                dp[i][j] = False
+            elif j == 0:
+                dp[i][j] = True
+            else:
+                if numbers[i-1] <= target:
+                    dp[i][j] = dp[i-1][j-numbers[i-1]] or dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j]
+
+    return dp[n][target]
+
+
+
 if __name__ == '__main__':
     numbers = [3, 34, 4, 12, 5, 2]
     target = 30
     n = len(numbers)
+    # Recursive way
     max_sum = subset_sum(numbers, target, n)
     if max_sum == target:
         print(True)
     else:
         print(False)
+
+    target = 9
+    # Bottom-up approach
+    print(subset_sum_bottom_up(numbers, target, n))
