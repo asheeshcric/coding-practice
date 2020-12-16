3a3ec8f06c3cf6d83dd5090de2cb61814b9c5c8f
asheeshcric <ashiz2013@gmail.com>
2020-12-16 17:35:21 -0600

    Find length of longest substring

---
+++ leetcode/5_longest_substring_without_repeating_chars.py
@@ -0,0 +1,22 @@
+def lengthOfLongestSubstring(s: str) -> int:
+    seen = []
+
+    count, max_count = 0, 0
+    for char in s:
+        if char not in seen:
+            seen.append(char)
+            count += 1
+        else:
+            # The char has already been seen
+            index = seen.index(char)
+            # Remove that char from seen and the ones before it
+            seen = seen[index+1:] if index+1 < len(seen) else []
+            seen.append(char)
+            count = len(seen)
+
+        max_count = max(max_count, count)
+
+    return max_count
+
+
+print(lengthOfLongestSubstring('dvdf'))
