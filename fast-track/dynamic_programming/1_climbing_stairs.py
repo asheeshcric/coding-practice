2b5f42276b28e43f031a95f5371829e5f9dbf746
asheeshcric <ashiz2013@gmail.com>
2021-07-02 17:01:09 -0500

    Reformat code

--- fast-track/dynamic_programming/1_climbing_stairs.py
+++ fast-track/dynamic_programming/1_climbing_stairs.py
@@ -4,9 +4,11 @@ You are climbing a staircase. It takes n steps to reach the top.
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 """
 
+
 def climb_stairs(n):
-    dp = [None for _ in range(n+1)]
+    dp = [None for _ in range(n + 1)]
     return check_steps(n, dp)
+
 
 def check_steps(n, dp):
     if n == 0:
@@ -14,8 +16,9 @@ def check_steps(n, dp):
     if n < 0:
         return 0
 
-    dp[n] = check_steps(n-1, dp) + check_steps(n-2, dp)
+    dp[n] = check_steps(n - 1, dp) + check_steps(n - 2, dp)
     return dp[n]
+
 
 if __name__ == "__main__":
     print(climb_stairs(5))
