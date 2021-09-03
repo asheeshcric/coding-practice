69be18e41b86123e452f7243825d1e768b308fc1
asheeshcric <ashiz2013@gmail.com>
2021-09-03 15:09:42 -0500

    Add comments to the functions

--- facebook/arrays_and_strings/1_longest_substring_without_repeating_chars.py
+++ facebook/arrays_and_strings/1_longest_substring_without_repeating_chars.py
@@ -1,4 +1,11 @@
 class Solution:
+    """
+    1. Keep track of the index where you see each char
+    2. Whenever you find a repeating char, make sure you mark that index+1 as the start of a new substring
+    and start counting the length from there. last_valid_index keeps track from where the substring starts.
+    3. Store the value of the max substring as you loop through the main string.
+    """
+
     def lengthOfLongestSubstring(self, s: str) -> int:
         seen_chars = {}
         max_length = 0
