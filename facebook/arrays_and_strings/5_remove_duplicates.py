e47ea5fe3d5432801e85c21719d5449d2963bd72
asheeshcric <ashiz2013@gmail.com>
2021-09-07 16:04:24 -0500

    Remove duplicates from array in place

---
+++ facebook/arrays_and_strings/5_remove_duplicates.py
@@ -0,0 +1,19 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        len_nums = len(nums)
+        if len_nums < 2:
+            return len_nums
+
+        new_i, old_i = 1, 1
+
+        while old_i < len(nums):
+            if nums[old_i] == nums[old_i-1]:
+                # Found a duplicate number
+                old_i += 1
+                continue
+
+            nums[new_i] = nums[old_i]
+            new_i += 1
+            old_i += 1
+
+        return new_i
