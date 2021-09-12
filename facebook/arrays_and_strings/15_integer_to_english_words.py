4d1f397bad66b2faab97250f1b694848ff707110
asheeshcric <ashiz2013@gmail.com>
2021-09-12 14:36:03 -0500

    Convert number to english words

---
+++ facebook/arrays_and_strings/15_integer_to_english_words.py
@@ -0,0 +1,84 @@
+"""
+Convert a non-negative integer num to its English words representation.
+Examples:
+
+Input: num = 123
+Output: "One Hundred Twenty Three"
+
+Input: num = 12345
+Output: "Twelve Thousand Three Hundred Forty Five"
+"""
+from collections import deque
+
+
+class Solution:
+    def numberToWords(self, num: int) -> str:
+        dic = {
+            1000000000: "Billion",
+            1000000: "Million",
+            1000: "Thousand",
+            100: "Hundred",
+            90: "Ninety",
+            80: "Eighty",
+            70: "Seventy",
+            60: "Sixty",
+            50: "Fifty",
+            40: "Forty",
+            30: "Thirty",
+            20: "Twenty",
+            19: "Nineteen",
+            18: "Eighteen",
+            17: "Seventeen",
+            16: "Sixteen",
+            15: "Fifteen",
+            14: "Fourteen",
+            13: "Thirteen",
+            12: "Twelve",
+            11: "Eleven",
+            10: "Ten",
+            9: "Nine",
+            8: "Eight",
+            7: "Seven",
+            6: "Six",
+            5: "Five",
+            4: "Four",
+            3: "Three",
+            2: "Two",
+            1: "One",
+            0: "Zero",
+        }
+        if num == 0:
+            return dic[0]
+
+        segments = [
+            float("inf"),
+            1000000000,
+            1000000,
+            1000,
+            100,
+            90,
+            80,
+            70,
+            60,
+            50,
+            40,
+            30,
+            20,
+        ]
+
+        def numberToWords(number):
+            nonlocal dic, segments
+            ret = deque()
+            for i in range(1, len(segments)):
+                if segments[i] <= number < segments[i - 1]:
+                    div, rest = number // segments[i], number % segments[i]
+                    ret.append(dic[segments[i]])
+                    if rest > 0:
+                        ret.append(numberToWords(rest))
+                    # We say "One Hundred", "One thoushand" but we don't say "One Fifty", we simply say "Fifty":
+                    if div > 0 and i < 5:
+                        ret.appendleft(numberToWords(div))
+                    return " ".join(ret)
+            return dic[number]
+
+        return numberToWords(num)
