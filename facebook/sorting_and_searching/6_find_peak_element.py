9edf72ca1ad2d02c7ba512fc0bfa4e0faf0de6d3
asheeshcric <ashiz2013@gmail.com>
2021-09-17 10:39:04 -0500

    Find peak element in an array

---
+++ facebook/sorting_and_searching/6_find_peak_element.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if mid+1 < len(nums) and nums[mid] < nums[mid+1]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left
