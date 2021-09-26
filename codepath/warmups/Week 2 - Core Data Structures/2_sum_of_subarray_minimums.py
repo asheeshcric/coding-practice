c21c5832cd9f76c42c796185b952089911010007
asheeshcric <ashiz2013@gmail.com>
2021-09-26 17:36:03 -0500

    Find the sum of subarray minimums

--- codepath/warmups/Week 2 - Core Data Structures/2_sum_of_subarray_minimums.py
+++ codepath/warmups/Week 2 - Core Data Structures/2_sum_of_subarray_minimums.py
@@ -1,17 +1,27 @@
+"""
+In this problem, we will require a monotonic non-decreasing stack to keep track of the number of items on the left of the current number
+
+For a particular number n, the total subarray sum value is: 
+    = n * (no. of numbers greater than n on the right + 1) * (no. of numbers greater than n on the left + 1)
+
+We need to find the above sum value for each number in the list and add them all to get the result
+"""
+
+
 from typing import List
 
 
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
-        total_sum = 0
-        for idx, num in enumerate(arr):
-            prefix = [num]
-            total_sum += self.subarray_sum(arr, idx, prefix)
+        result = 0
+        # Stack contains the indices of each number in the list
+        stack = []
+        arr = [float("-inf")] + arr + [float("-inf")]
+        for i, num in enumerate(arr):
+            while stack and arr[stack[-1]] > num:
+                curr = stack.pop()
+                result += arr[curr] * (i - curr) * (curr - stack[-1])
 
-        return total_sum
+            stack.append(i)
 
-    def subarray_sum(self, arr, idx, prefix) -> int:
-        current_sum = 0
-        # We start with the current prefix at "idx" and find more contiguous subarrays
-        for k in range(idx + 1, len(arr)):
-            pass
+        return result % (10 ** 9 + 7)
