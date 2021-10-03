40922b720a051b24ecd34bd5ffd07fcf10ed3544
asheeshcric <ashiz2013@gmail.com>
2021-10-02 21:26:06 -0500

    Format code

--- top_interview_questions/math/4_pow_x_n.py
+++ top_interview_questions/math/4_pow_x_n.py
@@ -1,3 +1,21 @@
+"""
+So, we start with a number x that needs to be raised to the power of n
+
+e.g: To evaluate 5 ^ 4,
+
+we can do: (5^2)*(5^2)
+or: (5^1)*(5^1)*(5^1)*(5^1)
+
+Base cases:
+if n == 0: return 1
+if x == 0: return 0
+
+result = self.recurse(x, n//2)
+result = result * result
+return result * x if n is odd else result
+"""
+
+
 class Solution:
     def recurse(self, x, n):
         if n == 0:
