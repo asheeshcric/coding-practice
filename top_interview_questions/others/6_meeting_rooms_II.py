972d7d52d862153663b82e1a9126f2ae6721cfda
asheeshcric <ashiz2013@gmail.com>
2021-10-04 14:12:46 -0500

    Find min number of rooms required based on the meeting times

---
+++ top_interview_questions/others/6_meeting_rooms_II.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        times = []
+        for start, end in intervals:
+            times.append((start, 1))
+            times.append((end, 0))
+
+        # If we encounter the same times, we want to first end the meetings if possible before starting a new one
+        times.sort(key=lambda x: (x[0], x[1]))
+
+        min_rooms = 0
+        curr_rooms = 0
+        for i, (time, start) in enumerate(times):
+            if start == 1:
+                curr_rooms += 1
+            else:
+                curr_rooms -= 1
+
+            min_rooms = max(min_rooms, curr_rooms)
+
+        return min_rooms
