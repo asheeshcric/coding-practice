f1a3350b0a852b529a7b2f7eb9ea7af6f6af769b
asheeshcric <ashiz2013@gmail.com>
2021-10-04 12:04:07 -0500

    Add a better method for moving zeroes to the end:

--- facebook/arrays_and_strings/16_move_zeroes.py
+++ facebook/arrays_and_strings/16_move_zeroes.py
@@ -12,20 +12,39 @@ from typing import List
 
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        for i, num in enumerate(nums):
-            if num != 0:
-                continue
+        if len(nums) < 2:
+            return
 
-            k = i + 1
-            while k < len(nums) and nums[k] == 0:
-                k += 1
+        i, j = 0, 1
+        while i < len(nums) and j < len(nums):
+            if nums[i] == 0:
+                # Need to replace this 0 with a non-zero number if exists
+                while j < len(nums) and nums[j] == 0:
+                    j += 1
 
-            if k == len(nums):
-                # This means we encountered all zeroes after index i. So, we don't need to change anything
-                break
+                if j >= len(nums):
+                    break
 
-            # Swap that element with the current zero
-            nums[i], nums[k] = nums[k], nums[i]
-            if not any(nums[k:]):
-                # This means there aren't any non-zero integers left to be swapped
-                break
+                # Swap 0 with the next non-zero number
+                nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j += 1
+
+    # def moveZeroes(self, nums: List[int]) -> None:
+    #     for i, num in enumerate(nums):
+    #         if num != 0:
+    #             continue
+
+    #         k = i + 1
+    #         while k < len(nums) and nums[k] == 0:
+    #             k += 1
+
+    #         if k == len(nums):
+    #             # This means we encountered all zeroes after index i. So, we don't need to change anything
+    #             break
+
+    #         # Swap that element with the current zero
+    #         nums[i], nums[k] = nums[k], nums[i]
+    #         if not any(nums[k:]):
+    #             # This means there aren't any non-zero integers left to be swapped
+    #             break
